2157e63b5090b010ce485a7d2e23c3abfff8ab7e
ntrevisa <nicolo.trevisani@cern.ch>
2023-06-27 17:53:23 +0200

    all variables but mt2. still need to check all definitions are correct

--- mkShapesRDF/processor/modules/l2KinProducer.py
+++ mkShapesRDF/processor/modules/l2KinProducer.py
@@ -1,5 +1,5 @@
 from mkShapesRDF.processor.framework.module import Module
-
+import ROOT
 
 class l2KinProducer(Module):
     def __init__(self):
@@ -35,37 +35,93 @@ class l2KinProducer(Module):
             excludeVariations=["JES*", "MET*"],
         )
 
+        df = df.Define(
+            "_isOk3l",
+            "Lepton_pt[Lepton_pt > 0].size() >= 3 && MET_4DV.E()>0",
+            excludeVariations=["JES*", "MET*"],
+        )
+
         df = df.Define("_lepOk", "Lepton_pt[Lepton_pt > 0].size()")
         df = df.Define("_tkMetOk", "TkMET_4DV.E() > 0")
         df = df.Define("_jetOk", "CleanJet_pt[CleanJet_pt > 0].size()")
 
+
         # FIXME complete l2kin module!
 
-        # Variables to be implemented:
+        # Variables yet to be implemented:
         ##############################
+        # 'mT2',
 
-        #            'mcoll',
-        #            'mcollWW',
-        #            'mTe',
-        #            'choiMass',
-        #            'mT2',
-        #            'channel',
-        #            'mllWgSt',
-        #            'drllWgSt',
-        #            'mllThird',
-        #            'mllOneThree',
-        #            'mllTwoThree',
-        #            'drllOneThree',
-        #            'drllTwoThree',
-        #            'PfMetDivSumMet',
-        #            'upara',
-        #            'uperp',
-        #            'vht_pt',
-        #            'vht_phi',
-        #            'WlepMt_whss'
+        # Additional supporting variables
+        #################################
 
-        # dilepton variables
+        # Needed for mcoll
+        df = df.Define(
+            "Lepton_enhanced_0",
+            "_isOk ? ROOT::Math::PtEtaPhiMVector(Lepton_pt[0] + MET_4DV.Pt() * cos( abs( DeltaPhi( Lepton_4DV[0].Phi(),MET_4DV.Phi() ) ) ), Lepton_eta[0], Lepton_phi[0], 0.) : ROOT::Math::PtEtaPhiMVector(0., 0., 0., 0.)"
+        )
+        df = df.Define(
+            "Lepton_enhanced_1",
+            "_isOk ? ROOT::Math::PtEtaPhiMVector(Lepton_pt[1] + MET_4DV.Pt() * cos( abs( DeltaPhi( Lepton_4DV[1].Phi(),MET_4DV.Phi() ) ) ), Lepton_eta[1], Lepton_phi[1], 0.) : ROOT::Math::PtEtaPhiMVector(0., 0., 0., 0.)"
+        )
+
+        # Needed for mcollWW
+        df = df.Define(
+            "Lepton_enhanced_WW_0",
+            "_isOk ? ROOT::Math::PtEtaPhiMVector(Lepton_pt[0] + MET_4DV.Pt() * cos( abs( DeltaPhi( Lepton_4DV[0].Phi(),MET_4DV.Phi() ) ) ), Lepton_eta[0], Lepton_phi[0], 80.385) : ROOT::Math::PtEtaPhiMVector(0., 0., 0., 0.)"
+        )
+        df = df.Define(
+            "Lepton_enhanced_WW_1",
+            "_isOk ? ROOT::Math::PtEtaPhiMVector(Lepton_pt[1] + MET_4DV.Pt() * cos( abs( DeltaPhi( Lepton_4DV[1].Phi(),MET_4DV.Phi() ) ) ), Lepton_eta[1], Lepton_phi[1], 80.385) : ROOT::Math::PtEtaPhiMVector(0., 0., 0., 0.)"
+        )
+
+        # Needed for mTe
+        df = df.Define(
+            "Lepton_enhanced_mTe_0",
+            "_isOk ? ROOT::Math::PtEtaPhiMVector(Lepton_pt[0], Lepton_eta[0], Lepton_phi[0], 80.385) : ROOT::Math::PtEtaPhiMVector(0., 0., 0., 0.)"
+        )
+        df = df.Define(
+            "Lepton_enhanced_mTe_1",
+            "_isOk ? ROOT::Math::PtEtaPhiMVector(Lepton_pt[1], Lepton_eta[1], Lepton_phi[1], 80.385) : ROOT::Math::PtEtaPhiMVector(0., 0., 0., 0.)"
+        )
+        
+        # Needed for mllWgSt or drllWgSt
+        df = df.Define(
+            "mll12",
+            "_isOk && (Lepton_pdgId[0] / Lepton_pdgId[1]) == -1 ? (Lepton_4DV[0] + Lepton_4DV[1]).M() : -9999.0"
+        )
+        df = df.Define(
+            "mll13",
+            "_isOk3l && (Lepton_pdgId[0] / Lepton_pdgId[2]) == -1 ? (Lepton_4DV[0] + Lepton_4DV[2]).M() : -9999.0"
+        )
+        df = df.Define(
+            "mll23",
+            "_isOk3l && (Lepton_pdgId[1] / Lepton_pdgId[2]) == -1 ? (Lepton_4DV[1] + Lepton_4DV[2]).M() : -9999.0"
+        )
+        
+        df = df.Define(
+            "drll12",
+            "_isOk ? DeltaR(Lepton_eta[0], Lepton_eta[1], Lepton_phi[0], Lepton_phi[1]) : -9999.0",
+        )
+        df = df.Define(
+            "drll13",
+            "_isOk3l ? DeltaR(Lepton_eta[0], Lepton_eta[2], Lepton_phi[0], Lepton_phi[2]) : -9999.0",
+        )
+        df = df.Define(
+            "drll23",
+            "_isOk3l ? DeltaR(Lepton_eta[1], Lepton_eta[2], Lepton_phi[1], Lepton_phi[2]) : -9999.0",
+        )
+
+        # Needed for upara and uperp
+        df = df.Define(
+            "utv",
+            "_isOk ? -MET_4DV- Lepton_4DV[0] - Lepton_4DV[1] : ROOT::Math::PtEtaPhiMVector(0., 0., 0., 0.)",
+        )
+        
+
+        # Dilepton variables
         prefix = "new_fw_"
+
         df = df.Define(
             prefix + "mll", "_isOk ? (Lepton_4DV[0] + Lepton_4DV[1]).M() : -9999.0"
         )
@@ -175,6 +231,80 @@ class l2KinProducer(Module):
             prefix + "recoil",
             "_isOk ? abs((Lepton_4DV[0] + Lepton_4DV[1] + MET_4DV).Pt()) : -9999.0",
         )
+        df = df.Define(
+            prefix + "mcoll", "_isOk ? (Lepton_enhanced_0 + Lepton_enhanced_1).M() : -9999.0"
+        )
+        df = df.Define(
+            prefix + "mcollWW", "_isOk ? (Lepton_enhanced_WW_0 + Lepton_enhanced_WW_1).M() : -9999.0"
+        )
+        df = df.Define(
+            prefix + "mTe", "_isOk ? (Lepton_enhanced_mTe_0 + Lepton_enhanced_mTe_1 + MET_4DV).M() : -9999.0"
+        )
+        df = df.Define(
+            prefix + "choiMass", "_isOk ? sqrt( 2*(Lepton_4DV[0].Pt()*Lepton_4DV[0].Pt()) + 2*(Lepton_4DV[1].Pt()*Lepton_4DV[1].Pt()) + 3*(Lepton_4DV[0].Pt()*Lepton_4DV[1].Pt() + (MET_4DV.Pt())*(Lepton_4DV[0].Pt()+Lepton_4DV[1].Pt()) - MET_4DV.Pt()*ptll*cos(dphilmet)) - 2*(Lepton_4DV[1].Pt()*Lepton_4DV[1].Pt()*cos(dphill)) ) : -9999.0"
+        )
+        df = df.Define(
+            prefix + "channel", "_isOk ? (abs(Lepton_pdgId[0]) == 11 && abs(Lepton_pdgId[1]) == 11) ? 1 : (abs(Lepton_pdgId[0]) == 11 && abs(Lepton_pdgId[1]) == 13) ? 2 : (abs(Lepton_pdgId[0]) == 13 && abs(Lepton_pdgId[1]) == 11) ? 3 : (abs(Lepton_pdgId[0]) == 13 && abs(Lepton_pdgId[1]) == 13) ? 0 : -9999.0 : -9999.0"
+        )
+
+        df = df.Define(
+            prefix + "mllWgSt", "_isOk3l ? mll12 >= 0 && mll13 <  0 && mll23 <  0 ? mll12 :\
+                                           mll12 <  0 && mll13 >= 0 && mll23 <  0 ? mll13 : \
+                                           mll12 <  0 && mll13 <  0 && mll23 >= 0 ? mll23 : \
+                                           mll12 >= 0 && mll13 >= 0 && mll23 <  0 && mll12 <  mll13 ? mll12 : \
+                                           mll12 >= 0 && mll13 >= 0 && mll23 <  0 && mll12 >= mll13 ? mll13 : \
+                                           mll12 >= 0 && mll13 <  0 && mll23 >= 0 && mll12 <  mll23 ? mll12 : \
+                                           mll12 >= 0 && mll13 <  0 && mll23 >= 0 && mll12 >= mll23 ? mll23 : \
+                                           mll12 <  0 && mll13 >= 0 && mll23 >= 0 && mll13 <  mll23 ? mll13 : \
+                                           mll12 <  0 && mll13 >= 0 && mll23 >= 0 && mll13 >= mll23 ? mll23 : \
+                                           mll12 >= 0 && mll13 >= 0 && mll23 >= 0 && mll12 <  mll13 && mll12 <  mll23 ? mll12 : \
+                                           mll12 >= 0 && mll13 >= 0 && mll23 >= 0 && mll13 <  mll12 && mll13 <  mll23 ? mll13 : \
+                                           mll12 >= 0 && mll13 >= 0 && mll23 >= 0 && mll23 <  mll12 && mll23 <  mll13 ? mll23 : \
+                                           -9999.0 : -9999.0"
+        )
+
+        df = df.Define(
+            prefix + "drllWgSt", "_isOk3l ? mll12 >= 0 && mll13 <  0 && mll23 <  0 ? drll12 : \
+                                            mll12 <  0 && mll13 >= 0 && mll23 <  0 ? drll13 : \
+                                            mll12 <  0 && mll13 <  0 && mll23 >= 0 ? drll23 : \
+                                            mll12 >= 0 && mll13 >= 0 && mll23 <  0 && mll12 <  mll13 ? drll12 : \
+                                            mll12 >= 0 && mll13 >= 0 && mll23 <  0 && mll12 >= mll13 ? drll13 : \
+                                            mll12 >= 0 && mll13 <  0 && mll23 >= 0 && mll12 <  mll23 ? drll12 : \
+                                            mll12 >= 0 && mll13 <  0 && mll23 >= 0 && mll12 >= mll23 ? drll23 : \
+                                            mll12 <  0 && mll13 >= 0 && mll23 >= 0 && mll13 <  mll23 ? drll13 : \
+                                            mll12 <  0 && mll13 >= 0 && mll23 >= 0 && mll13 >= mll23 ? drll23 : \
+                                            mll12 >= 0 && mll13 >= 0 && mll23 >= 0 && mll12 <  mll13 && mll12 <  mll23 ? drll12 : \
+                                            mll12 >= 0 && mll13 >= 0 && mll23 >= 0 && mll13 <  mll12 && mll13 <  mll23 ? drll13 : \
+                                            mll12 >= 0 && mll13 >= 0 && mll23 >= 0 && mll23 <  mll12 && mll23 <  mll13 ? drll23 : \
+                                            -9999.0 : -9999.0"
+        )
+
+        df = df.Define(
+            prefix + "mllThird", "_isOk3l && Lepton_4DV[2].Pt() > 2 ? mll13 >= 0 && mll23 >= 0 && mll23 <  mll13 ? mll23 : \
+                                                                      mll13 >= 0 && mll23 >= 0 && mll23 >= mll13 ? mll13 : \
+                                                                      mll13 >= 0 && mll23 <  0 ? mll13 : \
+                                                                      mll13 <  0 && mll23 >= 0 ? mll23 : \
+                                                                      -9999.0: -9999.0"
+        )
+
+        df = df.Define(
+            prefix + "mllOneThree", "_isOk3l && Lepton_4DV[2].Pt() > 2 ? mll13 : -9999.0"
+        )
+        df = df.Define(
+            prefix + "mllTwoThree", "_isOk3l && Lepton_4DV[2].Pt() > 2 ? mll23 : -9999.0"
+        )
+        df = df.Define(
+            prefix + "drllOneThree", "_isOk3l && Lepton_4DV[2].Pt() > 2 ? drll13 : -9999.0"
+        )
+        df = df.Define(
+            prefix + "drllTwoThree", "_isOk3l && Lepton_4DV[2].Pt() > 2 ? drll23 : -9999.0"
+        )
+        df = df.Define(
+            prefix + "upara", "_isOk ? utv.P() * cos(DeltaPhi(utv.Phi(), (Lepton_4DV[0] + Lepton_4DV[1]).Phi() )) : -9999.0"
+        )
+        df = df.Define(
+            prefix + "uperp", "_isOk ? utv.P() * sin(DeltaPhi(utv.Phi(), (Lepton_4DV[0] + Lepton_4DV[1]).Phi() )) : -9999.0"
+        )
 
         df = df.Define(prefix + "pt1", "Lepton_pt[0] > 0 ? Lepton_pt[0] : -9999.0")
         df = df.Define(prefix + "eta1", "Lepton_pt[0] > 0 ? Lepton_eta[0] : -9999.0")
@@ -269,6 +399,16 @@ class l2KinProducer(Module):
             "_isOk && _jetOk>=2 && CleanJet_4DV[1].Pt()>15.0 ? abs(DeltaPhi(CleanJet_4DV[1].Phi(), MET_4DV.Phi())) : -9999.0",
         )
 
+        df = df.Define(
+            prefix + "WlepMt_whss",
+            "_isOk ? _jetOk>=2 && {0}dphilep1jj   >  {0}dphilep2jj   ? sqrt( 2 * Lepton_4DV[0].Pt() * MET_4DV.Pt() * (1 - cos({0}dphilmet1) ) ) : \
+                     _jetOk>=2 && {0}dphilep1jj   <= {0}dphilep2jj   ? sqrt( 2 * Lepton_4DV[1].Pt() * MET_4DV.Pt() * (1 - cos({0}dphilmet2) ) ) : \
+                     _jetOk==1 && {0}dphilep1jet1 >  {0}dphilep2jet1 ? sqrt( 2 * Lepton_4DV[0].Pt() * MET_4DV.Pt() * (1 - cos({0}dphilmet1) ) ) : \
+                     _jetOk==1 && {0}dphilep1jet1 <= {0}dphilep2jet1 ? sqrt( 2 * Lepton_4DV[1].Pt() * MET_4DV.Pt() * (1 - cos({0}dphilmet2) ) ) : \
+            -9999.0 : -9999.0".format(prefix),
+        )
+
+
         # dijets variables
         df = df.Define(
             prefix + "njet",
@@ -322,6 +462,36 @@ class l2KinProducer(Module):
             prefix + "ht",
             "_isOk ? Sum(Lepton_pt[Lepton_pt>0]) + Sum(CleanJet_pt[CleanJet_pt>30]) + MET_4DV.Pt() : -9999.0",
         )
+        df = df.Define(
+            prefix + "PfMetDivSumMet",
+            "_isOk && PuppiMET_sumEt>0.0  ? MET_4DV.Pt() / sqrt(PuppiMET_sumEt) : -9999.0",
+        )
+
+        ROOT.gInterpreter.Declare("""
+        ROOT::Math::PtEtaPhiMVector Get_vht(ROOT::RVecF lep_pt, ROOT::RVecF lep_eta, ROOT::RVecF lep_phi, ROOT::RVecF jet_pt, ROOT::RVecF jet_eta, ROOT::RVecF jet_phi){
+            ROOT::Math::PtEtaPhiMVector vector_sum(0.,0.,0.,0.);
+            for (uint i = 0; i < lep_pt.size(); i++){
+                if (lep_pt[i] < 10) break;
+                ROOT::Math::PtEtaPhiMVector lepp4(lep_pt[i], lep_eta[i], lep_phi[i], 0);
+                vector_sum += lepp4;
+            }
+            for (uint i = 0; i < jet_pt.size(); i++){
+                if (jet_pt[i] < 30) break;
+                ROOT::Math::PtEtaPhiMVector jetp4(jet_pt[i], jet_eta[i], jet_phi[i], 0);
+                vector_sum += jetp4;
+            }
+        return vector_sum;
+        }
+        """)
+
+        df = df.Define(
+            prefix + "vht_pt",
+            "_isOk ? (Get_vht(Lepton_pt, Lepton_eta, Lepton_phi, CleanJet_pt, CleanJet_eta, CleanJet_phi)).Pt() : -9999.0",
+        )
+        # df = df.Define(
+        #     prefix + "vht_phi",
+        #     "_isOk ? (Get_vht(Lepton_pt, Lepton_eta, Lepton_phi, CleanJet_pt, CleanJet_eta, CleanJet_phi)).Phi() : -9999.0",
+        # )
 
         # For VBF training
         df = df.Define(
@@ -397,8 +567,25 @@ class l2KinProducer(Module):
         df = df.DropColumns("TkMET_4DV")
 
         df = df.DropColumns("_isOk")
+        df = df.DropColumns("_isOk3l")
         df = df.DropColumns("_lepOk")
         df = df.DropColumns("_tkMetOk")
         df = df.DropColumns("_jetOk")
+
+        df = df.DropColumns("Lepton_enhanced_0")
+        df = df.DropColumns("Lepton_enhanced_1")
+        df = df.DropColumns("Lepton_enhanced_WW_0")
+        df = df.DropColumns("Lepton_enhanced_WW_1")
+        df = df.DropColumns("Lepton_enhanced_mTe_0")
+        df = df.DropColumns("Lepton_enhanced_mTe_1")
+        df = df.DropColumns("utv")
+
+        df = df.DropColumns("mll12")
+        df = df.DropColumns("mll13")
+        df = df.DropColumns("mll23")
+        df = df.DropColumns("drll12")
+        df = df.DropColumns("drll13")
+        df = df.DropColumns("drll23")
+
 
         return df
